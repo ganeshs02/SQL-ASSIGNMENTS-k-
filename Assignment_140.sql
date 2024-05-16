f8cde96abd9ab68405075f89ffdfd60f87d1ae3d
Ganesh Surle <110679789+ganeshs02@users.noreply.github.com>
2024-05-16 12:16:40 +0530

    Update Assignment_140.sql

--- Assignment_140.sql
+++ Assignment_140.sql
@@ -1,6 +1,7 @@
 -- Generate report showing product _id,description,month,monthly_sale,previous_month_sale
 USE retail;
-SELECT *, LAG(monthly_sale) OVER(PARTITION BY sub.product_id ORDER BY sub.product_id) FROM 
+SELECT *, LAG(monthly_sale) OVER(PARTITION BY sub.product_id ORDER BY sub.product_id) 
+  FROM 
 (SELECT td.product_id ,p.description ,
   MONTH(td.tran_dt) AS month,SUM(td.amt) AS monthly_sale
 FROM tran_dtl td 
